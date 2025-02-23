3be18737e34bfd49b1380e4e6dacbc108c413b09
Sithum Sandaruwan <139766672+sithum-sandaruwan@users.noreply.github.com>
2025-02-23 13:09:21 +0530

    Initial commit

---
+++ backend/personal-to-do/src/main/java/com/sithum/todoapp/personal_to_do/PersonalToDoApplication.java
@@ -0,0 +1,13 @@
+package com.sithum.todoapp.personal_to_do;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalToDoApplication.class, args);
+	}
+
+}
---
+++ backend/personal-to-do/src/test/java/com/sithum/todoapp/personal_to_do/PersonalToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sithum.todoapp.personal_to_do;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
