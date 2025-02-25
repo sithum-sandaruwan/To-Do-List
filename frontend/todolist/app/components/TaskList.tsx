3b1cdb0f5720e6593cabdfaee650b7b1717f7792
Sithum Sandaruwan <139766672+sithum-sandaruwan@users.noreply.github.com>
2025-02-25 17:15:47 +0530

    Responsived the page components.

--- frontend/todolist/app/components/TaskList.tsx
+++ frontend/todolist/app/components/TaskList.tsx
@@ -1,10 +1,13 @@
 const TaskList = () => {
   return (
     <>
-      <div className="bg-white drop-shadow-lg rounded-md sm:w-max  p-11  m-6 ">
+      <div className=" bg-white drop-shadow-lg rounded-md   p-11  m-6 hover:">
         <h2 className="text-xl font-semibold mb-3">Task Title</h2>
         <p className="text-gray-800">
-          waivbaiwbiabwvbwdvibasdvasudvabsodvbsdbvoasbdvosbvobadvbasbdvobasodvbasdb
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          dolore, optio sunt pariatur voluptatum, neque vero ipsam saepe,
+          repellat consectetur labore asperiores. Nam dolores ex unde nostrum
+          impedit commodi recusandae?
         </p>
         <button className=" bg-green-500 p-2 w-24 mt-5 hover:bg-slate-200 rounded-md text-black drop-shadow-md">
           Done
--- frontend/todolist/app/page.tsx
+++ frontend/todolist/app/page.tsx
@@ -3,15 +3,17 @@ import TaskList from "./components/TaskList";
 
 export default function Home() {
   return (
-    <main className=" bg-white min-h-screen p-1  ">
-      <h1 className="text-center text-3xl m-6 transition-shadow text-sky-600  ">
+    <main className=" bg-white min-h-screen p-2  ">
+      <h1 className="text-center text-3xl m-6  text-sky-600  ">
         My To Do List
       </h1>
-      <div className="grid grid-cols-2 row-span-12 gap-2 ml-2">
-        <div className="flex flex-col mt-32 min-h-48 items-center rounded-md">
+
+      {/* Main Grid */}
+      <div className="flex flex-col md:grid md:grid-cols-2 gap-4 px-4 ">
+        <div className="flex flex-col  p-4 items-center rounded-md w-full">
           <AddTask />
         </div>
-        <div className="bg-gray-400 rounded-md max-w-36w-full  ">
+        <div className=" bg-gray-400 rounded-md p-4 shadow-md w-full  ">
           <TaskList />
         </div>
       </div>
