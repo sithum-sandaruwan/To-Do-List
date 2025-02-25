43470ee8c1ccffbd1f8f220ba8cfe6fd1c20ad42
Sithum Sandaruwan <139766672+sithum-sandaruwan@users.noreply.github.com>
2025-02-25 15:17:05 +0530

    Create the cards for task list.And setup the body color to white.

--- frontend/todolist/app/page.tsx
+++ frontend/todolist/app/page.tsx
@@ -3,15 +3,15 @@ import TaskList from "./components/TaskList";
 
 export default function Home() {
   return (
-    <main className=" bg-white min-h-screen ">
-      <h1 className="text-center text-3xl m-4 transition-shadow text-sky-600  ">
+    <main className=" bg-white min-h-screen p-1  ">
+      <h1 className="text-center text-3xl m-6 transition-shadow text-sky-600  ">
         My To Do List
       </h1>
       <div className="grid grid-cols-2 row-span-12 gap-2 ml-2">
         <div className="flex flex-col mt-32 min-h-48 items-center rounded-md">
           <AddTask />
         </div>
-        <div className="bg-slate-700  rounded-md mr-2">
+        <div className="bg-gray-400 rounded-md max-w-36w-full  ">
           <TaskList />
         </div>
       </div>
