895722727d9d290229accb9eaacb8dd64b8dee44
Sithum Sandaruwan <139766672+sithum-sandaruwan@users.noreply.github.com>
2025-02-26 23:52:04 +0530

    Task Controller & Task Service files created : get the JSON of task details from frontend create task function onvert it to the object.

---
+++ backend/personal-to-do/src/main/java/com/sithum/todoapp/personal_to_do/service/TaskService.java
@@ -0,0 +1,12 @@
+package com.sithum.todoapp.personal_to_do.service;
+
+import org.springframework.scheduling.config.Task;
+
+public class TaskService {
+
+    public Task createTask(Task task) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createTask'");
+    }
+
+}
