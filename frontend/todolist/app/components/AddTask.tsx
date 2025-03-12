d63f24cf4f0b198dc6275e899d477048b887430b
Sithum Sandaruwan <139766672+sithum-sandaruwan@users.noreply.github.com>
2025-03-12 21:17:51 +0530

    align the text fields and add task button.

--- frontend/todolist/app/components/AddTask.tsx
+++ frontend/todolist/app/components/AddTask.tsx
@@ -43,11 +43,11 @@ const AddTask = () => {
   };
 
   return (
-    <>
-      <form onSubmit={submitHandle}>
+    <div className="flex flex-col items-center  min-h-screen  p-4">
+      <form onSubmit={submitHandle} className="w-full max-w-lg">
         <input
           type="text"
-          className="p-4 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 rounded bg-slate-600 text-zinc-200 border-x-black shadow-md hover "
+          className="p-4 w-full  rounded bg-slate-600 text-zinc-200 border-x-black shadow-md hover mb-4"
           placeholder="What's Your New Task...?"
           value={task}
           onChange={(e) => setTask(e.target.value)}
@@ -55,14 +55,14 @@ const AddTask = () => {
         />
         <input
           type="text"
-          className="p-20 w-full sm:w-3/4 md:w-2/3 lg:w-1/2  rounded bg-slate-600 text-zinc-200 border-x-black shadow-md hover mt-4 ml-1"
+          className="p-20 w-full   rounded bg-slate-600 text-zinc-200 border-x-black shadow-md hover mb-4 "
           placeholder="Write Something About Task"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
         <input
           type="datetime-local"
-          className="p-20 w-full sm:w-3/4 md:w-2/3 lg:w-1/2  rounded bg-slate-600 text-zinc-200 border-x-black shadow-md hover mt-4 ml-1"
+          className="p-4 w-full rounded bg-slate-600 text-zinc-200 border-x-black shadow-md hover mb-4"
           placeholder="What will be the end date of the task..?"
           value={endDate}
           onChange={(e) => setEndDate(e.target.value)}
@@ -71,12 +71,12 @@ const AddTask = () => {
 
         <button
           type="submit"
-          className="p-2 rounded m-10 w-full sm:w-auto bg-red-600  text-white hover:drop-shadow-lg "
+          className="p-4 rounded m-10 w-full sm:w-auto bg-red-600  text-white hover:drop-shadow-lg transition-colors"
         >
           Add new task
         </button>
       </form>
-    </>
+    </div>
   );
 };
 
